95e1979b58d1ef1574b67cd5f7c68e797d6284b6
Patrick <patrickplays12@yahoo.com>
2018-12-31 15:33:42 +0000

    ads and seperate scripts

---
+++ video.js
@@ -0,0 +1,8 @@
+    function go_get(){
+     var base_url = 'http://www.youtube.com/embed?listType=search&list=';
+     var search_field = document.getElementById('yourtextfield').value;
+     var target_url = base_url + search_field;
+     var ifr = document.getElementById('youriframe');
+     ifr.src = target_url;
+     return false;
+    }
