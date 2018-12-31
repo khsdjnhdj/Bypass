94702552a5abb3bef83ba00f7329b115ba454adc
Patrick <patrickplays12@yahoo.com>
2018-12-31 15:39:53 +0000

    Update video.js

--- video.js
+++ video.js
@@ -1,8 +1,9 @@
-    function go_get(){
-     var base_url = 'http://www.youtube.com/embed?listType=search&list=';
+function go_get()
+{
+     var base_url = 'http://www.youtube.com/';
      var search_field = document.getElementById('yourtextfield').value;
      var target_url = base_url + search_field;
      var ifr = document.getElementById('youriframe');
      ifr.src = target_url;
      return false;
-    }
+}
